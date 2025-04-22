5f39172ee62c8f8f305ba671528e33ce964282ca
whatsmynameagain <43839098+whatsmynameagain@users.noreply.github.com>
2025-04-21 22:46:55 -0300

    CH2 L1 complete

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 
 	const rootDir = "./"
 	const port = "8080"
+
+	apiCfg := &apiConfig{} // fileserverHits default is 0, no need to initialize
 
 	newMux := http.NewServeMux()
 	serverStruct := &http.Server{
@@ -17,7 +21,11 @@ func main() {
 	}
 
 	fileServer := http.FileServer(http.Dir(rootDir))
-	newMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	newMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+
+	newMux.HandleFunc("/metrics", apiCfg.requestCountHandler)
+
+	newMux.HandleFunc("/reset", apiCfg.resetCountHandler)
 
 	newMux.HandleFunc("/healthz", readinessHandler)
 
@@ -37,4 +45,30 @@ func readinessHandler(w http.ResponseWriter, _ *http.Request) {
 	// ignore bytes, error return values
 	w.Write([]byte("OK"))
 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// still can't wrap my head around how this works
+// gotta come back to it later
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) requestCountHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	respText := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	w.Write([]byte(respText))
+}
+
+func (cfg *apiConfig) resetCountHandler(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset"))
 }
