a93b06bcc9c784d375ee03cd98443f02ca088524
whatsmynameagain <43839098+whatsmynameagain@users.noreply.github.com>
2025-04-27 18:04:44 -0300

    CH4 L2 start

---
+++ json_helper.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// shamelessly taken from the blog
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
