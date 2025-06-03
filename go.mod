cfebf0cc50adcf0de537ac3848cbc83adc5d9682
whatsmynameagain <43839098+whatsmynameagain@users.noreply.github.com>
2025-06-02 21:36:30 -0300

    CH6 L6 step 2

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require (
 )
 
 require golang.org/x/crypto v0.38.0
+
+require github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -2,7 +2,11 @@ package auth
 
 import (
 	"fmt"
+	"log"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -17,4 +21,33 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+
+	type CustomClaims struct {
+		jwt.RegisteredClaims
+	}
+
+	claims := CustomClaims{
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+			Subject:   userID.String(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedString, err := token.SignedString(tokenSecret)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return signedString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	return uuid.UUID{}, nil
 }
